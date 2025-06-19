abff990c739fb6773ed1e7059fdcd3a2cdd6af08
Bruno <david.lopez9926@gmail.com>
2025-06-19 00:33:45 -0500

    comp table

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,10 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ["1096-186-168-102-111.ngrok-free.app"]
+    }
   },
 
   integrations: [react()]
